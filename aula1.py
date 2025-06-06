232c7aa2e0f734a54035ce0c8badcfb81c38c95d
joaosantos86 <joaops41224@gmail.com>
2025-06-06 09:15:34 -0300

    Começar aula 1 sobre py

--- aula1.py
+++ aula1.py
@@ -1,1 +1,14 @@
-print("hello world")
+"""
+docsstring
+
+Permite comentar mais de uma linha
+"""
+
+'''
+Outra forma de comentar mais de uma linha
+'''
+
+# Permite comentar varias linhas
+print(123) #na frente 
+#Depois
+print(456) #na frente
